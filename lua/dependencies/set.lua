5c82a0be5eec69823e92daadf57ab34e54c7672e
Spheny1 <kylenwpb+github@gmail.com>
2024-02-07 14:32:53 -0500

    neovim conf with lazy

---
+++ lua/dependencies/set.lua
@@ -0,0 +1,12 @@
+vim.opt.nu = true
+vim.opt.wrap = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.incsearch = true
+
+
+vim.opt.scrolloff = 12
